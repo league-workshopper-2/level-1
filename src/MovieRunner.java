d74dabc2398408dfc8588c8a5d41843d489992c6
Cole <league@WTS5.local>
2016-08-31 17:00:29 -0700

    hi

---
+++ src/MovieRunner.java
@@ -0,0 +1,12 @@
+
+public class MovieRunner {
+	public static void main(String[] args) {
+
+		Movie james=new Movie("James Bond",1);
+		Movie deadpool=new Movie("Dead Pool",3);
+		Movie spy=new Movie("Spy",2);
+		Movie neighbors=new Movie("neighbors",3);
+		Movie neighbors2=new Movie("neighbors 2",3);
+		String JamesPrice=james.getTicketPrice();
+	}
+}
